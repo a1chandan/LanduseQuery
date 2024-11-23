29ab2071a80b4d2f75071b40c625ed05c943e91e
a1chandan <a1chandan@gmail.com>
2024-11-23 12:53:39 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const csvUrl = "https://raw.githubusercontent.com/a1chandan/LanduseQuery/refs/heads/main/kolvi_v2.csv";
+const csvUrl = "https://raw.githubusercontent.com/a1chandan/LanduseQuery/refs/heads/main/kolvi_v3.csv";
 let csvData = [];
 
 // Fetch CSV data on page load
